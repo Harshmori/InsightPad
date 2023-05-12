1c0dbb9ff08bdc3bede43b4b4e890c28c3296789
Harsh Mori <46750824+Harshmori@users.noreply.github.com>
2023-05-12 22:06:56 +0530

    Update settings.py

--- insightpad/settings.py
+++ insightpad/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-io(ybwm3!d!^cubav+txqbovcsb8vl0dm(q4j3!tji(b!89508
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
@@ -123,6 +123,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
